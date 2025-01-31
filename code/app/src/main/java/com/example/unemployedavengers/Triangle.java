412201d190170fd183f07d45cb092bbf21fe9802
wackyquacky <129981528+wackyquacky@users.noreply.github.com>
2025-01-30 19:47:14 -0700

    Add triangle class extending shape

--- code/app/src/main/java/com/example/unemployedavengers/Triangle.java
+++ code/app/src/main/java/com/example/unemployedavengers/Triangle.java
@@ -8,7 +8,7 @@ public class Triangle extends Shape {
     }
 
     @Override
-    public double getArea() {return 0.5f*x*y;}
+    public double getArea() {return 0.5f*x*y;}//gets the area of the triangle
 
 
 }
