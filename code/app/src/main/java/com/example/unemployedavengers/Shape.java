1e2073bca16f55f2e0f79041a45a8b97385bd341
Ethan Shi <ethanshi322@gmail.com>
2025-01-30 18:56:33 -0700

    initial

---
+++ code/app/src/main/java/com/example/unemployedavengers/Shape.java
@@ -0,0 +1,28 @@
+package com.example.unemployedavengers;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+    protected String color = "blue";
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Abstract method to be implemented by subclasses
+    public abstract double getArea();
+
+    // Getter and Setter for color
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void display() {
+        System.out.println("Shape at (" + x + ", " + y + ") with color: " + color);
+    }
+}
